071c1966b78d42aa233d93782006d5aa89ae12c4
aya015757881 <2581015450@qq.com>
2023-04-07 15:53:14 +0800

    refact: shorten package names for public usage

--- lib.rs
+++ lib.rs
@@ -1,5 +1,5 @@
-pub extern crate anychain_bitcoin;
+pub extern crate anychain_bitcoin as bitcoin;
 pub extern crate anychain_core as core;
-pub extern crate anychain_ethereum;
-pub extern crate anychain_filecoin;
-pub extern crate anychain_tron;
+pub extern crate anychain_ethereum as ethereum;
+pub extern crate anychain_filecoin as filecoin;
+pub extern crate anychain_tron as tron;
