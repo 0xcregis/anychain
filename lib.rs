bf9d4b6cbe1e2763bbea6110d5ef3bdce97ad006
aya015757881 <2581015450@qq.com>
2023-04-13 15:17:59 +0800

    create module for Ripple chain

--- lib.rs
+++ lib.rs
@@ -2,4 +2,6 @@ pub extern crate anychain_bitcoin as bitcoin;
 pub extern crate anychain_core as core;
 pub extern crate anychain_ethereum as ethereum;
 pub extern crate anychain_filecoin as filecoin;
+pub extern crate anychain_polkadot as polkadot;
+pub extern crate anychain_ripple as ripple;
 pub extern crate anychain_tron as tron;
