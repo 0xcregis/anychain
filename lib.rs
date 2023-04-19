834cbd1d9f555c78cb21737057df1b416d06abc1
aya015757881 <2581015450@qq.com>
2023-04-19 14:16:59 +0800

    delete: ripple module

--- lib.rs
+++ lib.rs
@@ -3,5 +3,4 @@ pub extern crate anychain_core as core;
 pub extern crate anychain_ethereum as ethereum;
 pub extern crate anychain_filecoin as filecoin;
 pub extern crate anychain_polkadot as polkadot;
-pub extern crate anychain_ripple as ripple;
 pub extern crate anychain_tron as tron;
