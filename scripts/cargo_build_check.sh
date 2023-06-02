e3ced1158957807021ac9132ecc28244b294b7c0
cairo <shuimuliang@gmail.com>
2023-06-03 01:36:37 +0800

    bump: toolchain update to 1.70.0 #30 (#33)

--- scripts/cargo_build_check.sh
+++ scripts/cargo_build_check.sh
@@ -6,7 +6,7 @@ set -e
 cd ..
 
 cargo fmt -- --check
-#cargo clippy --workspace --all-targets --tests -- -D warnings
+cargo clippy --workspace --all-targets --tests -- -D warnings
 cargo test --all-features
 
 exit 0
