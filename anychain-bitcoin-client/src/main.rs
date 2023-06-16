a5397775fcb90c4823bfc5f5d75da5a34c23ec07
aya015757881 <2581015450@qq.com>
2023-06-16 17:05:16 +0800

    add: anychain-bitcoin use case in anychain-bitcoin-client

--- anychain-bitcoin-client/src/main.rs
+++ anychain-bitcoin-client/src/main.rs
@@ -7,28 +7,28 @@ use anychain_bitcoin::{
         BitcoinTransaction, BitcoinTransactionInput, BitcoinTransactionOutput,
         BitcoinTransactionParameters,
     },
-    Bitcoin as Mainnet, BitcoinAddress, BitcoinFormat,
+    BitcoinTestnet as Testnet, BitcoinAddress, BitcoinFormat,
 };
 
 use anychain_core::{hex, libsecp256k1, Address, PublicKey, Transaction};
 
 fn address_from_secret_key() {
     let secret_key = [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 71, 1, 1, 1, 1, 8, 1, 1, 1, 111, 1, 1, 103, 1, 1, 57, 1, 1,
         1, 1,
     ];
 
     let secret_key = libsecp256k1::SecretKey::parse(&secret_key).unwrap();
 
     let addr_p2pkh =
-        BitcoinAddress::<Mainnet>::from_secret_key(&secret_key, &BitcoinFormat::P2PKH).unwrap();
+        BitcoinAddress::<Testnet>::from_secret_key(&secret_key, &BitcoinFormat::P2PKH).unwrap();
 
     let addr_p2sh_p2wpkh =
-        BitcoinAddress::<Mainnet>::from_secret_key(&secret_key, &BitcoinFormat::P2SH_P2WPKH)
+        BitcoinAddress::<Testnet>::from_secret_key(&secret_key, &BitcoinFormat::P2SH_P2WPKH)
             .unwrap();
 
     let addr_bech32 =
-        BitcoinAddress::<Mainnet>::from_secret_key(&secret_key, &BitcoinFormat::Bech32).unwrap();
+        BitcoinAddress::<Testnet>::from_secret_key(&secret_key, &BitcoinFormat::Bech32).unwrap();
 
     println!("address p2pkh = {}", addr_p2pkh);
     println!("address p2sh_p2wpkh = {}", addr_p2sh_p2wpkh);
@@ -43,7 +43,7 @@ fn address_from_public_key() {
 
     let public_key = libsecp256k1::PublicKey::parse_compressed(&public_key).unwrap();
 
-    let public_key = BitcoinPublicKey::<Mainnet>::from_secp256k1_public_key(public_key, true);
+    let public_key = BitcoinPublicKey::<Testnet>::from_secp256k1_public_key(public_key, true);
 
     let addr_p2pkh = public_key.to_address(&BitcoinFormat::P2PKH).unwrap();
 
@@ -57,17 +57,17 @@ fn address_from_public_key() {
 }
 
 fn address_from_str() {
-    let addr = "1C6Rc3w25VHud3dLDamutaqfKWqhrLRTaD";
+    let addr = "mm21MpCm2cVYBxZvxk6DaQC7C4o5Ukq2Wf";
 
-    let addr = BitcoinAddress::<Mainnet>::from_str(addr).unwrap();
+    let addr = BitcoinAddress::<Testnet>::from_str(addr).unwrap();
 
     println!("\naddress = {}", addr);
 }
 
 fn address_validation() {
-    let addr = "1C6Rc3w25VHud3dLDamutaqfKWqhrLRTaD";
+    let addr = "mm21MpCm2cVYBxZvxk6DaQC7C4o5Ukq2Wf";
 
-    let status = BitcoinAddress::<Mainnet>::is_valid(addr);
+    let status = BitcoinAddress::<Testnet>::is_valid(addr);
 
     println!("status = {}", status);
 }
@@ -81,28 +81,72 @@ fn amount_gen() {
 }
 
 fn transaction_gen() {
-    let input = BitcoinTransactionInput::new(
-        hex::decode("211e7bf8fcfd7d530e4ea3bb3d9fb11914e76bb98c1d34b7704bb898b943bb24").unwrap(),
-        1,
-        Some(BitcoinAddress::<Mainnet>::from_str("1C6Rc3w25VHud3dLDamutaqfKWqhrLRTaD").unwrap()),
-        Some(BitcoinAmount::from_btc(1).unwrap()),
-    )
-    .unwrap();
+    let secret_key = [
+        1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 71, 1, 1, 1, 1, 8, 1, 1, 1, 111, 1, 1, 103, 1, 1, 57, 1, 1,
+        1, 1,
+    ];
+    let secret_key = libsecp256k1::SecretKey::parse(&secret_key).unwrap();
+    let public_key = libsecp256k1::PublicKey::from_secret_key(&secret_key).serialize_compressed().to_vec();
 
-    let output = BitcoinTransactionOutput::new(
-        BitcoinAddress::<Mainnet>::from_str("1JF7WT1RoBRrwmNBnzSsqHG63rusDrqQNy").unwrap(),
-        BitcoinAmount::from_ubtc(1000).unwrap(),
-    )
-    .unwrap();
+    let recipient = "2MsRNMaKe8YWcdUaRi8jwa2aHG85kzsbUHe";
+    let amount = 500000;
+    let fee = 1000;
 
-    let mut tx = BitcoinTransaction::new(
-        &BitcoinTransactionParameters::new(vec![input], vec![output]).unwrap(),
-    )
-    .unwrap();
+    let inputs = [
+        (
+            "39f420dc156f4ac1ad753a9fae1206973d9eede39a004c04496b7f9f525c77b8",
+            0,
+            "mm21MpCm2cVYBxZvxk6DaQC7C4o5Ukq2Wf",
+            1378890
+        ),
+        (
+            "312afea64f1efeefc6bdf73827daeee99ff025c9f1dc036bb62ff708c4eedcad",
+            0,
+            "2NCwYikg4pdCGPjgiK3T4Y1DW6Dnp5eobfy",
+            1818565
+        ),
+        (
+            "dc163eb31a9cdd5a8bb49066477375f9a0068791176e7b4a61e54751581449ae",
+            1,
+            "tb1q83t5qrd4yzrd477eskjp5f8ujtrf6enwgw87rn",
+            1481548
+        )
+    ];
 
-    let _hash = tx.digest(0).unwrap();
+    for i in 0..inputs.len() {
+        let input = BitcoinTransactionInput::new(
+            hex::decode(inputs[i].0).unwrap(),
+            inputs[i].1,
+            Some(BitcoinAddress::<Testnet>::from_str(inputs[i].2).unwrap()),
+            Some(BitcoinAmount::from_satoshi(inputs[i].3).unwrap()),
+        )
+        .unwrap();
 
-    println!("tx = {}", tx);
+        let output1 = BitcoinTransactionOutput::new(
+            BitcoinAddress::<Testnet>::from_str(recipient).unwrap(),
+            BitcoinAmount::from_satoshi(amount).unwrap(),
+        )
+        .unwrap();
+
+        let output2 = BitcoinTransactionOutput::new(
+            BitcoinAddress::<Testnet>::from_str(inputs[i].2).unwrap(),
+            BitcoinAmount::from_satoshi(inputs[i].3 - amount - fee).unwrap(),
+        )
+        .unwrap();
+
+        let mut tx = BitcoinTransaction::new(
+            &BitcoinTransactionParameters::new(vec![input], vec![output1, output2]).unwrap(),
+        )
+        .unwrap();
+
+        let hash = tx.digest(0).unwrap();
+        let msg = libsecp256k1::Message::parse_slice(&hash).unwrap();
+        let sig = libsecp256k1::sign(&msg, &secret_key).0.serialize().to_vec();
+
+        let _ = tx.sign(sig, public_key.clone(), i as u32);
+
+        println!("tx = {}\n", tx);
+    }
 }
 
 fn main() {
