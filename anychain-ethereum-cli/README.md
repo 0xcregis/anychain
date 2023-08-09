221c30d184b767182dc77757ddb73f3e52527458
kreg <kreg@is.dev>
2023-08-09 13:14:08 +0800

    feat: anychain-ethereum-cli binary #79

---
+++ anychain-ethereum-cli/README.md
@@ -0,0 +1,124 @@
+# anychain-ethereum-cli
+
+A command line program for the generation and/or validation of addresses and transactions of evm compatible blockchains
+
+### Build
+
+cd anychain/anychain-ethereum-cli
+
+cargo build --release
+
+
+
+### Run
+```
+cd ../target/release
+```
+
+#### 0. Generate Address 
+
+##### from public key
+
+```bash
+./anychain-ethereum-cli address-gen --pub 0x0299bb27e93fba02d13d78b1d7807cc811266e31e7b6feae3e09d42d49482fda95
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+./anychain-ethereum-cli address-gen --pub 0299bb27e93fba02d13d78b1d7807cc811266e31e7b6feae3e09d42d49482fda95
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+./anychain-ethereum-cli address-gen --pub 0499bb27e93fba02d13d78b1d7807cc811266e31e7b6feae3e09d42d49482fda9519127e684ded784c50d4559d87e0723c801635f6c387bb36cb4bf72e33201934
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+./anychain-ethereum-cli address-gen --pub 0x0499bb27e93fba02d13d78b1d7807cc811266e31e7b6feae3e09d42d49482fda9519127e684ded784c50d4559d87e0723c801635f6c387bb36cb4bf72e33201934
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+```
+
+##### from private key
+
+```bash
+./anychain-ethereum-cli address-gen --prv 0xb2ab8cfd1de774907de3fb12c8f3bafb76a575e1004f9f33309f1c570be18baf
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+./anychain-ethereum-cli address-gen --prv b2ab8cfd1de774907de3fb12c8f3bafb76a575e1004f9f33309f1c570be18baf
+0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+```
+
+#### 1. Parse Address from String & Validate Address
+
+```bash
+./anychain-ethereum-cli address-validate E0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+Valid
+./anychain-ethereum-cli address-validate 0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1814
+Valid
+./anychain-ethereum-cli address-validate 0xE0709D3C521fe51CA7E65E00929D7EEFCF0b1815
+Valid # anychain does not check the checksum
+./anychain-ethereum-cli address-validate 0xE0709D3C521fe51CA7E65E00929D7EEFCF0b18145
+Invalid
+```
+
+#### 2. Generate wei from ether and vice versa
+
+```bash
+./anychain-ethereum-cli towei 0.12345678ether
+123456780000000000
+./anychain-ethereum-cli towei 1gwei
+1000000000
+./anychain-ethereum-cli towei 1.1Gwei
+1100000000
+```
+
+
+#### 3. Make Transaction
+
+This is a "Do What I Mean" (DWIM) style command, which will generate a transaction from the given parameters.
+The parameters are:
+
+| your intent | mode | to | token | value | data | rsv |
+| ----------- | ---- | -- | ----- | ----- | ---- | --- |
+| unsigned ether transfer | main | recepient | leave empty | amount in wei | leave empty | leave empty |
+| unsigned token transfer | erc20 | recepient | token contract | amount of token | leave empty | leave empty |
+| unsigned any transaction | any | to | leave empty | value | data | leave empty |
+| signed ether transfer | main | recepient | leave empty | amount in wei | leave empty | rsv |
+| signed token transfer | erc20 | recepient | token contract | amount of token | leave empty | rsv |
+| signed any transaction | any | to | leave empty | value | data | rsv |
+
+
+##### Make an ether transaction's preimage
+
+```bash
+./anychain-ethereum-cli maketx --mode main --network sepolia --nonce 4 --gasprice 2gwei --gaslimit 21000 --to 0x717648D7d50fF001cc1088E8dc416Dfa26c32CB9 --value 1145141919810893 
+```
+which outputs:
+```
+tx = 0xed04847735940082520894717648d7d50ff001cc1088e8dc416dfa26c32cb9870411802159054d8083aa36a78080
+```
+
+##### Make an signed ether transaction
+
+```bash
+./anychain-ethereum-cli maketx --mode main --network sepolia --nonce 4 --gasprice 2gwei --gaslimit 21000 --to 0x717648D7d50fF001cc1088E8dc416Dfa26c32CB9 --value 1145141919810893 -r 371efe039d17f0bf74b732c94a5d7c4a04f4bf747bde94a779dd8fd7b180d106 -s 0eacd1b265ddfea09f4c20400429dba2224af8eed462a732358f7da8f1848e18 -v 0
+```
+which outputs:
+```
+tx = 0xf86e04847735940082520894717648d7d50ff001cc1088e8dc416dfa26c32cb9870411802159054d808401546d71a0371efe039d17f0bf74b732c94a5d7c4a04f4bf747bde94a779dd8fd7b180d106a00eacd1b265ddfea09f4c20400429dba2224af8eed462a732358f7da8f1848e18
+```
+
+this tx can successfully broadcast: https://sepolia.etherscan.io/tx/0x9abef6656c8c246af1e68d5bca943507727b10597fd2b1cf84d14a868e7da3fb
+
+##### make an erc-20 transaction's preimage
+
+```bash
+./anychain-ethereum-cli maketx --mode erc20 --network sepolia --nonce 5 --gasprice 2gwei --gaslimit 51000 --to 0x717648D7d50fF001cc1088E8dc416Dfa26c32CB9 --token 0x779877A7B0D9E8603169DdbD7836e478b4624789 --value 1145141919810893 
+```
+which outputs
+```
+tx = 0xf86b05847735940082c73894779877a7b0d9e8603169ddbd7836e478b462478980b844a9059cbb000000000000000000000000717648d7d50ff001cc1088e8dc416dfa26c32cb9000000000000000000000000000000000000000000000000000411802159054d83aa36a78080
+```
+
+##### make an signed erc-20 transaction
+
+```bash
+./anychain-ethereum-cli maketx --mode erc20 --network sepolia --nonce 5 --gasprice 2gwei --gaslimit 51000 --to 0x717648D7d50fF001cc1088E8dc416Dfa26c32CB9 --token 0x779877A7B0D9E8603169DdbD7836e478b4624789 --value 1145141919810893 -r 2fe49c0b946b77bb2c95a60a57c2ec9e5cb25504d0e549eb3a3cbfed8bc9cc54 -s 0x0e050e046dec65c1505653fe6147eb1fa20c63f6b8c7b78c7138b74f48a260a1 -v 0
+```
+which outputs
+```
+tx = 0xf8ac05847735940082c73894779877a7b0d9e8603169ddbd7836e478b462478980b844a9059cbb000000000000000000000000717648d7d50ff001cc1088e8dc416dfa26c32cb9000000000000000000000000000000000000000000000000000411802159054d8401546d71a02fe49c0b946b77bb2c95a60a57c2ec9e5cb25504d0e549eb3a3cbfed8bc9cc54a00e050e046dec65c1505653fe6147eb1fa20c63f6b8c7b78c7138b74f48a260a1
+```
+this tx can successfully broadcast: https://sepolia.etherscan.io/tx/0x9646b0732437f7f004798fb6ce923c61030934796e8f77db748d9ef32e68b597
+
---
+++ anychain-ethereum-cli/src/main.rs
@@ -0,0 +1,353 @@
+use clap::value_parser;
+use clap::{Arg, Command};
+use primitive_types::H160;
+use primitive_types::U256;
+use rlp::Encodable;
+use std::fmt::Display;
+use std::fmt::Error;
+use std::fmt::Formatter;
+use std::str::FromStr;
+
+use anychain_ethereum::{encode_transfer, EthereumAddress, EthereumFormat, EthereumPublicKey};
+
+use anychain_core::{hex, libsecp256k1, Address, PublicKey};
+
+fn trim0x(s: &str) -> String {
+    // str to lower case
+    let s = s.to_lowercase();
+    // remove 0x prefix
+    let s = s.trim_start_matches("0x");
+    s.to_string()
+}
+
+struct EIP155Transaction {
+    nonce: u64,
+    gas_price: U256,
+    gas_limit: u64,
+    to: H160,
+    value: U256,
+    data: Vec<u8>,
+    v: u64, // {0, 1} + chain_id * 2 + 35, since EIP-155
+    r: U256,
+    s: U256,
+}
+
+impl Encodable for EIP155Transaction {
+    fn rlp_append(&self, s: &mut rlp::RlpStream) {
+        s.begin_list(9);
+        s.append(&self.nonce);
+        s.append(&self.gas_price);
+        s.append(&self.gas_limit);
+        s.append(&self.to);
+        s.append(&self.value);
+        s.append(&self.data);
+        s.append(&self.v);
+        s.append(&self.r);
+        s.append(&self.s);
+    }
+}
+
+impl Display for EIP155Transaction {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let encoded = rlp::encode(self);
+        let hex = hex::encode(&encoded);
+        write!(f, "0x{}", hex)
+    }
+}
+
+// impl Serialize for EIP155Transaction {
+// 	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+// 		where
+// 			S: Serializer {
+
+// 		serializer.serialize_str(&hex)
+// 	}
+// }
+
+fn main() {
+    let matches = Command::new("anychain")
+        .subcommands(vec![
+            Command::new("address-gen")
+                .about("Generate an address ethereum-compatible blockchain")
+                .arg(
+                    Arg::new("private_key")
+                        .long("prv")
+                        .num_args(1)
+                        .help("Generate an address from a private key in hex format"),
+                )
+                .arg(
+                    Arg::new("public_key")
+                        .long("pub")
+                        .num_args(1)
+                        .help("Generate an address from a public key in hex format"),
+                ),
+            Command::new("address-validate")
+                .about("Check if an address is valid")
+                .arg(
+                    Arg::new("address")
+                        .num_args(1)
+                        .help("Specify the address to validate"),
+                ),
+            Command::new("towei")
+                .about("Convert ether/gwei/wei to wei")
+                .arg(
+                    Arg::new("value")
+                        .num_args(1)
+                        .help("Specify the ether to be converted"),
+                ),
+            Command::new("maketx")
+                .about("Generate an EIP-155 transaction")
+                .arg(
+                    Arg::new("mode")
+                        .long("mode")
+                        .num_args(1)
+                        .help("transaction mode: main / erc20 / any"),
+                )
+                .arg(
+                    Arg::new("network")
+                        .long("network")
+                        .num_args(1)
+                        .help("Specify the network, according to https://chainid.network/"),
+                )
+                .arg(
+                    Arg::new("nonce")
+                        .long("nonce")
+                        .num_args(1)
+                        .value_parser(value_parser!(u64))
+                        .help("nonce of the sender"),
+                )
+                .arg(
+                    Arg::new("gas_price")
+                        .long("gasprice")
+                        .num_args(1)
+                        .help("gas price of the transaction"),
+                )
+                .arg(
+                    Arg::new("gas_limit")
+                        .long("gaslimit")
+                        .num_args(1)
+                        .value_parser(value_parser!(u64))
+                        .help("gas limit of the transaction"),
+                )
+                .arg(
+                    Arg::new("to")
+                        .long("to")
+                        .num_args(1)
+                        .help("address of the receiver"),
+                )
+                .arg(
+                    Arg::new("token")
+                        .long("token")
+                        .num_args(1)
+                        .help("address of the token contract (erc20)"),
+                )
+                .arg(
+                    Arg::new("value")
+                        .long("value")
+                        .num_args(1)
+                        .value_parser(value_parser!(u64))
+                        .help("value of the transaction"),
+                )
+                .arg(
+                    Arg::new("data")
+                        .long("data")
+                        .num_args(1)
+                        .help("data of the transaction"),
+                )
+                .arg(
+                    Arg::new("r")
+                        .long("r")
+                        .short('r')
+                        .num_args(1)
+                        .default_value("0")
+                        .help("r of the signature"),
+                )
+                .arg(
+                    Arg::new("s")
+                        .long("s")
+                        .short('s')
+                        .num_args(1)
+                        .default_value("0")
+                        .help("s of the signature"),
+                )
+                .arg(
+                    Arg::new("v")
+                        .long("v")
+                        .short('v')
+                        .num_args(1)
+                        .default_value("0")
+                        .value_parser(value_parser!(u64))
+                        .help("v of the signature"),
+                ),
+        ])
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("address-gen", sub_matches)) => {
+            let private_key = sub_matches.get_one::<String>("private_key");
+            let public_key = sub_matches.get_one::<String>("public_key");
+            let public_key = match (private_key, public_key) {
+                (Some(_), Some(_)) => {
+                    println!("Both private key and public key are provided");
+                    return;
+                }
+                (Some(private_key), None) => {
+                    let private_key = trim0x(private_key);
+                    if private_key.len() != 64 {
+                        println!("Invalid private key");
+                        return;
+                    }
+                    let private_key = hex::decode(private_key).unwrap();
+                    let private_key = libsecp256k1::SecretKey::parse_slice(&private_key).unwrap();
+                    libsecp256k1::PublicKey::from_secret_key(&private_key)
+                }
+                (None, Some(public_key)) => {
+                    let public_key = trim0x(public_key);
+                    let public_key = hex::decode(public_key).unwrap();
+                    // parse_slice can handle both compressed and uncompressed public key
+                    // therefore we don't need to check the length and prefix of the public key
+                    libsecp256k1::PublicKey::parse_slice(&public_key, None).unwrap()
+                }
+                (None, None) => {
+                    println!("No private key or public key is provided");
+                    return;
+                }
+            };
+            println!("{}", address_from_public_key(public_key))
+        }
+        Some(("address-validate", sub_matches)) => {
+            let address = sub_matches.get_one::<String>("address");
+
+            if address.is_none() {
+                println!("Address not provided");
+                return;
+            }
+            let address = address.unwrap();
+            if EthereumAddress::is_valid(address) {
+                println!("Valid");
+            } else {
+                println!("Invalid");
+            }
+        }
+        Some(("towei", sub_matches)) => {
+            let value = sub_matches.get_one::<String>("value");
+
+            if value.is_none() {
+                println!("Value not provided");
+                return;
+            }
+            let value = towei(value.unwrap());
+            match value {
+                Some(value) => println!("{}", value),
+                None => println!("Invalid value"),
+            }
+        }
+        Some(("maketx", sub_matches)) => {
+            let network = sub_matches.get_one::<String>("network").unwrap();
+            let chainid = network_to_chainid(network);
+            let nonce = *sub_matches.get_one::<u64>("nonce").unwrap();
+            let gas_price = sub_matches.get_one::<String>("gas_price").unwrap();
+            let gas_price = towei(gas_price).unwrap();
+            let gas_limit = *sub_matches.get_one::<u64>("gas_limit").unwrap();
+
+            let mut r = U256::from_str(sub_matches.get_one::<String>("r").unwrap()).unwrap();
+            let mut s = U256::from_str(sub_matches.get_one::<String>("s").unwrap()).unwrap();
+            let mut v = *sub_matches.get_one::<u64>("v").unwrap();
+            if r == U256::from(0) || s == U256::from(0) {
+                // unsigned
+                r = U256::from(0);
+                s = U256::from(0);
+                v = chainid;
+            } else {
+                // signed
+                v = v + chainid * 2 + 35; // according to EIP155, https://eips.ethereum.org/EIPS/eip-155
+            }
+
+            let to;
+            let value;
+            let data;
+            let mode = sub_matches.get_one::<String>("mode").unwrap();
+            match mode.as_str() {
+                "main" => {
+                    to = H160::from_str(sub_matches.get_one::<String>("to").unwrap()).unwrap(); // to is to
+                    value = U256::from(*sub_matches.get_one::<u64>("value").unwrap()); // value is value
+                    data = Vec::<u8>::new(); // data is empty
+                }
+                "erc20" => {
+                    to = H160::from_str(sub_matches.get_one::<String>("token").unwrap()).unwrap(); // to is token
+                    value = U256::from(0); // value is 0
+                    let recepient = sub_matches.get_one::<String>("to").unwrap();
+                    let recepient = EthereumAddress::from_str(recepient).unwrap();
+                    let amount = U256::from(*sub_matches.get_one::<u64>("value").unwrap());
+                    data = encode_transfer("transfer", &recepient, amount); // data is encoded transfer(recepient, amount)
+                }
+                "any" => {
+                    to = H160::from_str(sub_matches.get_one::<String>("to").unwrap()).unwrap(); // as is
+                    value = U256::from(*sub_matches.get_one::<u64>("value").unwrap()); // as is
+                    data = hex::decode(sub_matches.get_one::<String>("data").unwrap()).unwrap();
+                    // as is
+                }
+                _ => {
+                    println!("Invalid mode");
+                    return;
+                }
+            }
+
+            let tx = EIP155Transaction {
+                nonce,
+                gas_price,
+                gas_limit,
+                to,
+                value,
+                data,
+                v,
+                r,
+                s,
+            };
+            println!("tx = {}", tx);
+        }
+        _ => {}
+    };
+}
+
+fn address_from_public_key(public_key: libsecp256k1::PublicKey) -> EthereumAddress {
+    let public_key = EthereumPublicKey::from_secp256k1_public_key(public_key);
+    public_key.to_address(&EthereumFormat::Standard).unwrap()
+}
+
+fn network_to_chainid(network: &str) -> u64 {
+    // to lower
+    let network = network.to_lowercase();
+    match network.as_str() {
+        "eth" => 1,
+        "ethereum" => 1,
+        "etc" => 61,
+        "ethereum_classic" => 61,
+        "goerli" => 5,
+        "sepolia" => 11155111,
+        _ => panic!("Invalid network"),
+    }
+}
+
+fn towei(value: &str) -> Option<U256> {
+    let value = value.to_lowercase();
+    // println!("{}", value);
+    if value.ends_with("ether") {
+        let value = value.trim_end_matches("ether");
+        let value = value.parse::<f64>().unwrap();
+        let value = value * 1e18;
+        Some(U256::from(value as u128))
+    } else if value.ends_with("gwei") {
+        let value = value.trim_end_matches("gwei");
+        let value = value.parse::<f64>().unwrap();
+        let value = value * 1e9;
+        Some(U256::from(value as u128))
+    } else if value.ends_with("wei") {
+        let value = value.trim_end_matches("wei");
+        let value = value.parse::<f64>().unwrap();
+        println!("{}", value as u128);
+        Some(U256::from(value as u128))
+    } else {
+        None
+    }
+}
