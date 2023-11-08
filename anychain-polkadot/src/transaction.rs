116b7ef0b9b773a32977d7591e783731e2861585
2581015450 <2581015450@qq.com>
2023-11-08 11:36:54 +0800

    feat: add crate polkadot-scale-codec

--- anychain-polkadot/src/transaction.rs
+++ anychain-polkadot/src/transaction.rs
@@ -1,7 +1,13 @@
 use crate::{PolkadotAddress, PolkadotNetwork};
+use parity_scale_codec::Encode;
 
 
 pub struct PolkadotTransactionParameters<N: PolkadotNetwork> {
+    extrinsic_version: u8,
+    spec_version: u32,
+    tx_version: u32,
+    genesis_hash: Vec<u8>,
+    block_hash: Vec<u8>,
     from: PolkadotAddress<N>,
     to: PolkadotAddress<N>,
 
