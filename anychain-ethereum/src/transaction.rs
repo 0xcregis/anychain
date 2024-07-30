c1e11df1af0977ebdf3628bf26db4405bea9a037
aya015757881 <2581015450@qq.com>
2024-07-30 12:20:08 +0800

    refactor: straighten existing code logic

--- anychain-ethereum/src/transaction.rs
+++ anychain-ethereum/src/transaction.rs
@@ -66,24 +66,36 @@ pub fn encode_transfer(func_name: &str, address: &EthereumAddress, amount: U256)
     func.encode_input(&tokens).unwrap()
 }
 
-/// Represents the parameters for an Ethereum transaction
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct EthereumTransactionParameters {
-    /// The address of the receiver
-    pub receiver: EthereumAddress,
-    /// The amount (in wei)
-    pub amount: EthereumAmount,
-    /// The transaction gas limit
-    pub gas: U256,
-    /// The transaction gas price in wei
-    pub gas_price: EthereumAmount,
-    /// The nonce of the Ethereum account
     pub nonce: U256,
-    /// The transaction data
+    pub gas_price: EthereumAmount,
+    pub gas_limit: U256,
+    pub to: EthereumAddress,
+    pub amount: EthereumAmount,
     pub data: Vec<u8>,
 }
 
 impl EthereumTransactionParameters {
+    pub fn to_rlp(&self) -> Result<RlpStream, TransactionError> {
+        let to = self
+            .to
+            .to_bytes()
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+
+        let mut rlp = RlpStream::new();
+        rlp.begin_list(9);
+
+        rlp.append(&self.nonce);
+        rlp.append(&self.gas_price.0);
+        rlp.append(&self.gas_limit);
+        rlp.append(&to);
+        rlp.append(&self.amount.0);
+        rlp.append(&self.data);
+
+        Ok(rlp)
+    }
+
     pub fn decode_data(&self) -> Result<Value, TransactionError> {
         if self.data.len() < 4 {
             return Err(TransactionError::Message("Illegal data".to_string()));
@@ -135,18 +147,13 @@ impl EthereumTransactionParameters {
     }
 }
 
-/// Represents an Ethereum transaction signature
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EthereumTransactionSignature {
-    /// The V field of the signature protected with a chain_id
     pub v: Vec<u8>,
-    /// The R field of the signature
     pub r: Vec<u8>,
-    /// The S field of the signature
     pub s: Vec<u8>,
 }
 
-/// Represents an Ethereum transaction id
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EthereumTransactionId {
     pub txid: Vec<u8>,
@@ -160,16 +167,53 @@ impl fmt::Display for EthereumTransactionId {
     }
 }
 
-/// Represents an Ethereum transaction
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct EthereumTransaction<N: EthereumNetwork> {
     /// The address of the sender
     pub sender: Option<EthereumAddress>,
     /// The transaction parameters (gas, gas_price, nonce, data)
-    pub parameters: EthereumTransactionParameters,
+    pub params: EthereumTransactionParameters,
     /// The transaction signature
     pub signature: Option<EthereumTransactionSignature>,
     _network: PhantomData<N>,
+}
+
+impl<N: EthereumNetwork> EthereumTransaction<N> {
+    pub fn restore_sender(&mut self) -> Result<(), TransactionError> {
+        let sig = self.signature.clone().unwrap();
+        self.signature = None;
+
+        let v = sig.v.clone();
+        let r = sig.r.clone();
+        let s = sig.s.clone();
+
+        let v: [u8; 4] = v.try_into().unwrap();
+        let v = u32::from_be_bytes(v);
+        let recid = (v - N::CHAIN_ID * 2 - 35) as u8;
+        let recid = libsecp256k1::RecoveryId::parse(recid)
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+
+        let _sig = [r, s].concat();
+        let _sig = libsecp256k1::Signature::parse_standard_slice(&_sig)
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+
+        let msg = self.to_transaction_id()?.txid;
+        let msg = libsecp256k1::Message::parse_slice(&msg)
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+
+        let pk = libsecp256k1::recover(&msg, &_sig, &recid)
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+        let pk = EthereumPublicKey::from_secp256k1_public_key(pk);
+
+        let sender = pk
+            .to_address(&EthereumFormat::Standard)
+            .map_err(|e| TransactionError::Message(format!("{}", e)))?;
+
+        self.sender = Some(sender);
+        self.signature = Some(sig);
+
+        Ok(())
+    }
 }
 
 impl<N: EthereumNetwork> Transaction for EthereumTransaction<N> {
@@ -179,203 +223,111 @@ impl<N: EthereumNetwork> Transaction for EthereumTransaction<N> {
     type TransactionId = EthereumTransactionId;
     type TransactionParameters = EthereumTransactionParameters;
 
-    /// Returns an unsigned transaction given the transaction parameters.
-    fn new(parameters: &Self::TransactionParameters) -> Result<Self, TransactionError> {
+    fn new(params: &Self::TransactionParameters) -> Result<Self, TransactionError> {
         Ok(Self {
             sender: None,
-            parameters: parameters.clone(),
+            params: params.clone(),
             signature: None,
             _network: PhantomData,
         })
     }
 
-    /// Returns a signed transaction given the {r,s,recid}.
     fn sign(&mut self, rs: Vec<u8>, recid: u8) -> Result<Vec<u8>, TransactionError> {
-        let message = libsecp256k1::Message::parse_slice(&self.to_transaction_id()?.txid)
-            .map_err(|error| TransactionError::Crate("libsecp256k1", format!("{:?}", error)))?;
-        let recovery_id = libsecp256k1::RecoveryId::parse(recid)
-            .map_err(|error| TransactionError::Crate("libsecp256k1", format!("{:?}", error)))?;
-
-        let public_key = EthereumPublicKey::from_secp256k1_public_key(
-            libsecp256k1::recover(
-                &message,
-                &libsecp256k1::Signature::parse_standard_slice(rs.as_slice()).map_err(|error| {
-                    TransactionError::Crate("libsecp256k1", format!("{:?}", error))
-                })?,
-                &recovery_id,
-            )
-            .map_err(|error| TransactionError::Crate("libsecp256k1", format!("{:?}", error)))?,
-        );
-        self.sender = Some(public_key.to_address(&EthereumFormat::Standard)?);
-        self.signature = Some(EthereumTransactionSignature {
-            v: (u32::from(recid) + N::CHAIN_ID * 2 + 35)
-                .to_be_bytes()
-                .to_vec(), // EIP155
-            r: rs[..32].to_vec(),
-            s: rs[32..64].to_vec(),
-        });
+        if rs.len() != 64 {
+            return Err(TransactionError::Message(format!(
+                "Invalid signature length: {}",
+                rs.len()
+            )));
+        }
+        let v = u32::from(recid) + N::CHAIN_ID * 2 + 35;
+        let v = v.to_be_bytes().to_vec();
+        let r = rs[..32].to_vec();
+        let s = rs[32..].to_vec();
+        self.signature = Some(EthereumTransactionSignature { v, r, s });
         self.to_bytes()
     }
 
-    /// Returns a transaction given the transaction bytes.
-    /// <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md>
-    fn from_bytes(transaction: &[u8]) -> Result<Self, TransactionError> {
-        let list: Vec<Vec<u8>> = decode_list(transaction);
+    fn to_bytes(&self) -> Result<Vec<u8>, TransactionError> {
+        match &self.signature {
+            Some(sig) => {
+                let mut rlp = self.params.to_rlp()?;
+                let v = trim_leading_zeros(&sig.v);
+                let r = trim_leading_zeros(&sig.r);
+                let s = trim_leading_zeros(&sig.s);
+                rlp.append(&v);
+                rlp.append(&r);
+                rlp.append(&s);
+                Ok(rlp.out().to_vec())
+            }
+            None => {
+                let mut rlp = self.params.to_rlp()?;
+                let chain_id = N::CHAIN_ID.to_be_bytes().to_vec();
+                let chain_id = trim_leading_zeros(&chain_id);
+                rlp.append(&chain_id);
+                rlp.append(&0u8);
+                rlp.append(&0u8);
+                Ok(rlp.out().to_vec())
+            }
+        }
+    }
+
+    fn from_bytes(tx: &[u8]) -> Result<Self, TransactionError> {
+        let list: Vec<Vec<u8>> = decode_list(tx);
+
         if list.len() != 9 {
             return Err(TransactionError::InvalidRlpLength(list.len()));
         }
 
-        let parameters = EthereumTransactionParameters {
-            receiver: EthereumAddress::from_str(&hex::encode(&list[3]))?,
-            amount: match list[4].is_empty() {
-                true => EthereumAmount::from_u256(U256::zero()),
-                false => EthereumAmount::from_u256(U256::from(list[4].as_slice())),
-            },
-            gas: match list[2].is_empty() {
-                true => U256::zero(),
-                false => U256::from(list[2].as_slice()),
-            },
-            gas_price: match list[1].is_empty() {
-                true => EthereumAmount::from_u256(U256::zero()),
-                false => EthereumAmount::from_u256(U256::from(list[1].as_slice())),
-            },
-            nonce: match list[0].is_empty() {
-                true => U256::zero(),
-                false => U256::from(list[0].as_slice()),
-            },
+        let nonce = match list[0].is_empty() {
+            true => U256::zero(),
+            false => U256::from(list[0].as_slice()),
+        };
+
+        let gas_price = match list[1].is_empty() {
+            true => EthereumAmount::from_u256(U256::zero()),
+            false => EthereumAmount::from_u256(U256::from(list[1].as_slice())),
+        };
+
+        let gas_limit = match list[2].is_empty() {
+            true => U256::zero(),
+            false => U256::from(list[2].as_slice()),
+        };
+
+        let to = EthereumAddress::from_str(&hex::encode(&list[3]))?;
+
+        let amount = match list[4].is_empty() {
+            true => EthereumAmount::from_u256(U256::zero()),
+            false => EthereumAmount::from_u256(U256::from(list[4].as_slice())),
+        };
+
+        let params = EthereumTransactionParameters {
+            nonce,
+            gas_price,
+            gas_limit,
+            to,
+            amount,
             data: list[5].clone(),
         };
 
-        match list[7].is_empty() && list[8].is_empty() {
-            true => {
-                // Raw transaction
-                Ok(Self {
-                    sender: None,
-                    parameters,
-                    signature: None,
-                    _network: PhantomData,
-                })
-            }
-            false => {
-                // Signed transaction
-                let mut v = list[6].clone();
-                pad_zeros(&mut v, 4);
-                let v: [u8; 4] = v.try_into().unwrap();
-                let v = u32::from_be_bytes(v);
-                let recovery_id = libsecp256k1::RecoveryId::parse((v - N::CHAIN_ID * 2 - 35) as u8)
-                    .map_err(|error| {
-                        TransactionError::Crate("libsecp256k1", format!("{:?}", error))
-                    })?;
-                let mut r = list[7].clone();
-                pad_zeros(&mut r, 32);
-                let mut s = list[8].clone();
-                pad_zeros(&mut s, 32);
-                let signature = [r.clone(), s.clone()].concat();
-                let raw_transaction = Self {
-                    sender: None,
-                    parameters: parameters.clone(),
-                    signature: None,
-                    _network: PhantomData,
-                };
-                let message =
-                    libsecp256k1::Message::parse_slice(&raw_transaction.to_transaction_id()?.txid)
-                        .map_err(|error| {
-                            TransactionError::Crate("libsecp256k1", format!("{:?}", error))
-                        })?;
-                let public_key = EthereumPublicKey::from_secp256k1_public_key(
-                    libsecp256k1::recover(
-                        &message,
-                        &libsecp256k1::Signature::parse_standard_slice(signature.as_slice())
-                            .map_err(|error| {
-                                TransactionError::Crate("libsecp256k1", format!("{:?}", error))
-                            })?,
-                        &recovery_id,
-                    )
-                    .map_err(|error| {
-                        TransactionError::Crate("libsecp256k1", format!("{:?}", error))
-                    })?,
-                );
+        let mut tx = EthereumTransaction::<N>::new(&params)?;
 
-                Ok(Self {
-                    sender: Some(public_key.to_address(&EthereumFormat::Standard)?),
-                    parameters,
-                    signature: Some(EthereumTransactionSignature {
-                        v: v.to_be_bytes().to_vec(),
-                        r,
-                        s,
-                    }),
-                    _network: PhantomData,
-                })
-            }
+        if !list[7].is_empty() && !list[8].is_empty() {
+            let mut v = list[6].clone();
+            let mut r = list[7].clone();
+            let mut s = list[8].clone();
+            pad_zeros(&mut v, 4);
+            pad_zeros(&mut r, 32);
+            pad_zeros(&mut s, 32);
+            tx.signature = Some(EthereumTransactionSignature { v, r, s });
+            tx.restore_sender()?;
         }
+
+        Ok(tx)
     }
 
-    /// Returns the transaction in bytes.
-    /// <https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md>
-    fn to_bytes(&self) -> Result<Vec<u8>, TransactionError> {
-        // Returns an encoded transaction in Recursive Length Prefix (RLP) format.
-        // https://github.com/ethereum/wiki/wiki/RLP
-        fn encode_transaction(
-            transaction_rlp: &mut RlpStream,
-            parameters: &EthereumTransactionParameters,
-        ) -> Result<(), TransactionError> {
-            transaction_rlp.append(&parameters.nonce);
-            transaction_rlp.append(&parameters.gas_price.0);
-            transaction_rlp.append(&parameters.gas);
-            transaction_rlp.append(&hex::decode(&parameters.receiver.to_string()[2..])?);
-            transaction_rlp.append(&parameters.amount.0);
-            transaction_rlp.append(&parameters.data);
-            Ok(())
-        }
-
-        // Returns the raw transaction (in RLP).
-        fn raw_transaction<N: EthereumNetwork>(
-            parameters: &EthereumTransactionParameters,
-        ) -> Result<RlpStream, TransactionError> {
-            let mut transaction_rlp = RlpStream::new();
-            transaction_rlp.begin_list(9);
-            encode_transaction(&mut transaction_rlp, parameters)?;
-            let chain_id = N::CHAIN_ID.to_be_bytes().to_vec();
-            let chain_id = trim_leading_zeros(&chain_id);
-            transaction_rlp.append(&chain_id);
-            transaction_rlp.append(&0u8);
-            transaction_rlp.append(&0u8);
-            Ok(transaction_rlp)
-        }
-
-        // Returns the signed transaction (in RLP).
-        fn signed_transaction(
-            parameters: &EthereumTransactionParameters,
-            signature: &EthereumTransactionSignature,
-        ) -> Result<RlpStream, TransactionError> {
-            let mut transaction_rlp = RlpStream::new();
-            transaction_rlp.begin_list(9);
-            encode_transaction(&mut transaction_rlp, parameters)?;
-            // trim the leading zeros of v
-            let v = trim_leading_zeros(&signature.v);
-            transaction_rlp.append(&v);
-            // trim the leading zeros of r
-            let r = trim_leading_zeros(&signature.r);
-            transaction_rlp.append(&r);
-            // trim the leading zeros of s
-            let s = trim_leading_zeros(&signature.s);
-            transaction_rlp.append(&s);
-            Ok(transaction_rlp)
-        }
-
-        match &self.signature {
-            Some(signature) => Ok(signed_transaction(&self.parameters, signature)?
-                .out()
-                .to_vec()),
-            None => Ok(raw_transaction::<N>(&self.parameters)?.out().to_vec()),
-        }
-    }
-
-    /// Returns the hash of the signed transaction, if the signature is present.
-    /// Otherwise, returns the hash of the raw transaction.
     fn to_transaction_id(&self) -> Result<Self::TransactionId, TransactionError> {
         Ok(Self::TransactionId {
-            txid: Vec::<u8>::from(&keccak256(&self.to_bytes()?)[..]),
+            txid: keccak256(&self.to_bytes()?).to_vec(),
         })
     }
 }
@@ -403,4 +355,27 @@ impl<N: EthereumNetwork> fmt::Display for EthereumTransaction<N> {
             })
         )
     }
+}
+
+pub struct Eip1559TransactionSignature {
+    pub y_parity: bool,
+    pub r: [u8; 32],
+    pub s: [u8; 32],
+}
+
+pub struct Eip1559TransactionParameters {
+    pub chain_id: u32,
+    pub nonce: U256,
+    pub max_priority_fee_per_gas: EthereumAmount,
+    pub max_fee_per_gas: EthereumAmount,
+    pub gas_limit: U256,
+    pub to: EthereumAddress,
+    pub amount: EthereumAmount,
+    pub data: Vec<u8>,
+}
+
+pub struct Eip1559Transaction<N: EthereumNetwork> {
+    pub parameters: Eip1559TransactionParameters,
+    pub signature: Option<Eip1559TransactionSignature>,
+    _network: PhantomData<N>,
 }
