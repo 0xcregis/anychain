d786c20faa27db122c0ab79fac049aaf0ace172a
aya015757881 <2581015450@qq.com>
2025-08-14 15:27:00 +0800

    feat: add sei & cro for eth network

--- crates/anychain-ethereum/src/network/mainnet.rs
+++ crates/anychain-ethereum/src/network/mainnet.rs
@@ -39,67 +39,68 @@ pub struct Linea;
 #[derive(Copy, Clone, Debug)]
 pub struct Xlayer;
 
+#[derive(Copy, Clone, Debug)]
+pub struct Sei;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Cro;
+
 impl EthereumNetwork for Ethereum {
     const CHAIN_ID: u32 = 1;
-    const NETWORK_ID: u32 = 1;
 }
 
 impl EthereumNetwork for EthereumClassic {
     const CHAIN_ID: u32 = 61;
-    const NETWORK_ID: u32 = 61;
 }
 
 impl EthereumNetwork for Polygon {
     const CHAIN_ID: u32 = 137;
-    const NETWORK_ID: u32 = 137;
 }
 
 impl EthereumNetwork for Arbitrum {
     const CHAIN_ID: u32 = 42161;
-    const NETWORK_ID: u32 = 42161;
 }
 
 impl EthereumNetwork for Avalanche {
     const CHAIN_ID: u32 = 43114;
-    const NETWORK_ID: u32 = 43114;
 }
 
 impl EthereumNetwork for Base {
     const CHAIN_ID: u32 = 8453;
-    const NETWORK_ID: u32 = 8453;
 }
 
 impl EthereumNetwork for BinanceSmartChain {
     const CHAIN_ID: u32 = 56;
-    const NETWORK_ID: u32 = 56;
 }
 
 impl EthereumNetwork for HuobiEco {
     const CHAIN_ID: u32 = 128;
-    const NETWORK_ID: u32 = 128;
 }
 
 impl EthereumNetwork for Okex {
     const CHAIN_ID: u32 = 66;
-    const NETWORK_ID: u32 = 66;
 }
 
 impl EthereumNetwork for OpBnb {
     const CHAIN_ID: u32 = 204;
-    const NETWORK_ID: u32 = 204;
 }
 
 impl EthereumNetwork for Optimism {
     const CHAIN_ID: u32 = 10;
-    const NETWORK_ID: u32 = 10;
 }
 
 impl EthereumNetwork for Linea {
     const CHAIN_ID: u32 = 59144;
-    const NETWORK_ID: u32 = 59144;
 }
 
 impl EthereumNetwork for Xlayer {
     const CHAIN_ID: u32 = 196;
-    const NETWORK_ID: u32 = 196;
+}
+
+impl EthereumNetwork for Sei {
+    const CHAIN_ID: u32 = 1329;
+}
+
+impl EthereumNetwork for Cro {
+    const CHAIN_ID: u32 = 25;
 }
