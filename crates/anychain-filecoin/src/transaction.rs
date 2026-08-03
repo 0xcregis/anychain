3655ee82b5a72997c6571d266dc7fe8b50a926a7
loki-cmu <attackeng@gmail.com>
2026-08-03 17:37:27 +0800

    fix: sec deps

--- crates/anychain-filecoin/src/transaction.rs
+++ crates/anychain-filecoin/src/transaction.rs
@@ -213,7 +213,7 @@ impl TransactionId for FilecoinTransactionId {}
 
 impl fmt::Display for FilecoinTransactionId {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", &BASE32_ENCODER.encode(&self.txid[..]))
+        write!(f, "{}", BASE32_ENCODER.encode(&self.txid[..]))
     }
 }
 
