c9798530321e86f69c900be51924eabd8f9f83b3
Brian <2581015450@qq.com>
2026-08-10 17:12:15 +0800

    feat: anychain stellar #389  (#428)
    * feat: anychain-stellar amount
    
    * feat: anychain-stellar
    
    * build(deps): bump stellar-strkey from 0.0.17 to 0.0.18
    
    * fmt: straightening
    
    * fix: address string
    
    * feat: transaction
    
    * fix: public access for stellar transaction fields
    
    * fix: anychain-core dep
    
    * fix: transaction id
    
    * fix: stream serialization
    
    * fix: name simplification
    
    * fix: transfer to non-existing account
    
    * fix: nonce +1
    
    * style: space line
    
    * style: name simplification
    
    * style: cargo fmt
    
    ---------
    
    Co-authored-by: loki-cmu <attackeng@gmail.com>

---
+++ crates/anychain-stellar/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod address;
+pub mod amount;
+pub mod format;
+pub mod public_key;
+pub mod transaction;
+
+pub use self::address::*;
+pub use self::amount::*;
+pub use self::format::*;
+pub use self::public_key::*;
+pub use self::transaction::*;
